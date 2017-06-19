8dd3bb2416aa34f6e98cc31906967b3d7f40caa0
Daniel Iglesias <iglesias.dan@gmail.com>
2017-06-19 01:34:27 -0400

    fixing route

--- app.js
+++ app.js
@@ -27,6 +27,11 @@
                 controllerAs: 'vm'
             })
 
+            .when('/sao', {
+                templateUrl: '../SAO/Views/index.php#/inicio',
+                //controllerAs: 'vm'
+            })
+
             .otherwise({ redirectTo: '/login' });
     }
 
@@ -38,7 +43,14 @@
             $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
         }
 
-        
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/login');
+            }
+        });
     }
 
 })();
--- login/login.controller.js
+++ login/login.controller.js
@@ -43,7 +43,7 @@
                 if (response.success) {
                     console.log(vm.username);
                     AuthenticationService.SetCredentials(vm.username, vm.password);
-                    $location.path('../SAO/Views/index.php#/estudios/');
+                    $location.path('/sao');
                 } else {
                     FlashService.Error(response.message);
                     vm.dataLoading = false;
