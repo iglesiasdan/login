9a905be77846dbf8969b32bc05a4c3ba5a6d22b4
Daniel Iglesias <iglesias.dan@gmail.com>
2017-06-19 01:12:54 -0400

    init commit

---
+++ login/login.controller.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService','UserService', '$rootScope'];
+    function LoginController($location, AuthenticationService, FlashService, UserService, $rootScope) {
+        var vm = this;
+        var service = {};
+        service.username = vm.username;
+
+        vm.register = register;
+        function register() {
+            vm.dataLoading = true;
+            UserService.Create(service)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        //$location.path('/login');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+
+
+
+
+
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                if (response.success) {
+                    console.log(vm.username);
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+        };
+    }
+
+})();
